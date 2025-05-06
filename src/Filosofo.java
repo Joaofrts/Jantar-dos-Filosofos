ca7257426f212bdb8cb59cb3b6a055a022bcf3d9
João Marcelo <joao.marcelo.c17@gmail.com>
2025-05-06 12:57:16 -0300

    Adicionando contadores de pensamento e  comer

--- src/Filosofo.java
+++ src/Filosofo.java
@@ -1,47 +1,82 @@
 class Filosofo extends Thread {
     private final Garfo garfoEsquerdo;
     private final Garfo garfoDireito;
+    private Integer vezesComeu = 0;
+    private Integer vezesPensou = 0;
+    private Integer repeticoes ;
 
     public Filosofo(Garfo garfoEsquerdo, Garfo garfoDireito, String nome) {
-        super(nome); // Define nome da Thread
+        super(nome);
         this.garfoEsquerdo = garfoEsquerdo;
         this.garfoDireito = garfoDireito;
     }
 
+    public Filosofo(Garfo garfoEsquerdo, Garfo garfoDireito, String nome, Integer repeticao) {
+        super(nome);
+        this.garfoEsquerdo = garfoEsquerdo;
+        this.garfoDireito = garfoDireito;
+        this.repeticoes = repeticao;
+    }
+
     private void pensar() throws InterruptedException {
         System.out.println(getName() + " está pensando...");
-        Thread.sleep((long) (Math.random() * 1000)); // tempo aleatório
+        Thread.sleep((long) (Math.random() * 1000));
+        vezesPensou++;
     }
 
     private void comer() throws InterruptedException {
         System.out.println(getName() + " está comendo ...");
-        Thread.sleep((long) (Math.random() * 1000)); // tempo aleatório
+        Thread.sleep((long) (Math.random() * 1000));
+        vezesComeu++;
+    }
+
+    private void relatorio(){
+        System.out.println();
+        System.out.println(getName() + " comeu "+vezesComeu+" vezes.");
+        System.out.println("E pensou "+vezesPensou+" vezes.");
+        System.out.println();
+    }
+
+    private void executar() throws InterruptedException {
+        pensar();
+        synchronized (garfoEsquerdo) {
+            System.out.println(getName() + " pegou o garfo esquerdo " + garfoEsquerdo.getId());
+
+            synchronized (garfoDireito) {
+                System.out.println(getName() + " pegou o garfo direito " + garfoDireito.getId());
+
+                comer();
+
+                System.out.println(getName() + " largou o garfo direito " + garfoDireito.getId());
+            }
+
+            System.out.println(getName() + " largou o garfo esquerdo " + garfoEsquerdo.getId());
+        }
     }
 
     @Override
     public void run() {
         try {
-            while (true) {
-                pensar();
+            if(repeticoes != null){
+                for (int i = 0; i < repeticoes; i++) {
+                    executar();
+                }
+                relatorio();
 
-                synchronized (garfoEsquerdo) {
-                    System.out.println(getName() + " pegou o garfo esquerdo " + garfoEsquerdo.getId());
-
-                    synchronized (garfoDireito) {
-                        System.out.println(getName() + " pegou o garfo direito " + garfoDireito.getId());
-
-                        comer();
-
-                        System.out.println(getName() + " largou o garfo direito " + garfoDireito.getId());
-                    }
-
-                    System.out.println(getName() + " largou o garfo esquerdo " + garfoEsquerdo.getId());
+            }
+            else{
+                while (true) {
+                    executar();
                 }
             }
+
         } catch (InterruptedException e) {
             System.out.println(getName() + " foi interropido!");
+            relatorio();
             Thread.currentThread().interrupt();
         }
     }
+
+
 }
 
--- src/JantarDosFilosofos.java
+++ src/JantarDosFilosofos.java
@@ -1,25 +1,81 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class JantarDosFilosofos {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bem-vindo ao Jantar dos Filósofos!");
+        System.out.print("Deseja rodadas limitadas? (s/n): ");
+        String escolha = scanner.nextLine().trim().toLowerCase();
+
+        boolean rodadasLimitadas = escolha.equals("s");
+        int repeticoes = 0;
+
+        if (rodadasLimitadas) {
+            System.out.print("Quantas rodadas cada filósofo deve executar? ");
+            while (true) {
+                try {
+                    repeticoes = scanner.nextInt();
+                    scanner.nextLine();
+                    if (repeticoes > 0) {
+                        break;
+                    } else {
+                        System.out.print("Digite um número inteiro positivo: ");
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.print("Entrada inválida. Digite um número inteiro: ");
+                    scanner.nextLine();
+                }
+            }
+        }
+
+
         Garfo[] garfos = new Garfo[5];
         for (int i = 0; i < garfos.length; i++) {
             garfos[i] = new Garfo(i);
         }
+
 
         Filosofo[] filosofos = new Filosofo[5];
         for (int i = 0; i < filosofos.length; i++) {
             Garfo garfoEsquerdo = garfos[i];
             Garfo garfoDireito = garfos[(i + 1) % garfos.length];
 
-            // Para evitar deadlock:
-            if (i == filosofos.length - 1) {
-                // O último filósofo pega os garfos na ordem inversa
-                filosofos[i] = new Filosofo(garfoDireito, garfoEsquerdo, "Filósofo " + (i+1));
+            if (rodadasLimitadas) {
+
+                if (i == filosofos.length - 1) {
+                    filosofos[i] = new Filosofo(garfoDireito, garfoEsquerdo, "Filósofo " + (i + 1), repeticoes);
+                } else {
+                    filosofos[i] = new Filosofo(garfoEsquerdo, garfoDireito, "Filósofo " + (i + 1), repeticoes);
+                }
             } else {
-                filosofos[i] = new Filosofo(garfoEsquerdo, garfoDireito, "Filósofo " + (i+1));
+
+                if (i == filosofos.length - 1) {
+                    filosofos[i] = new Filosofo(garfoDireito, garfoEsquerdo, "Filósofo " + (i + 1));
+                } else {
+                    filosofos[i] = new Filosofo(garfoEsquerdo, garfoDireito, "Filósofo " + (i + 1));
+                }
             }
 
             filosofos[i].start();
         }
+
+        if (rodadasLimitadas) {
+            for (Filosofo filosofo : filosofos) {
+                try {
+                    filosofo.join();
+                } catch (InterruptedException e) {
+                    System.out.println("Main foi interrompida enquanto esperava os filósofos.");
+                }
+            }
+            System.out.println("Todos os filósofos terminaram suas rodadas!");
+        } else {
+            System.out.println("Os filósofos estão rodando infinitamente.");
+        }
+
+        scanner.close();
     }
 }
+
 
